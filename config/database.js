8fe37343d0bcd78fdf9bdd3c78d7de41aea3031d
Jonathan Dey <jon@trenttech.co.uk>
2022-08-17 13:43:53 +0100

    New content type

--- config/database.js
+++ config/database.js
@@ -4,10 +4,13 @@ module.exports = ({ env }) => ({
     connection: {
       host: env('DATABASE_HOST', '127.0.0.1'),
       port: env.int('DATABASE_PORT', 3306),
-      database: env('DATABASE_NAME', 'strapi_demo'),
-      user: env('DATABASE_USERNAME', ''),
-      password: env('DATABASE_PASSWORD', ''),
-      ssl: env.bool('DATABASE_SSL', false),
+      database: env('DATABASE_NAME', 'strapi'),
+      user: env('DATABASE_USERNAME', 'strapi'),
+      password: env('DATABASE_PASSWORD', 'strapi'),
+      ssl: {
+        rejectUnauthorized: env.bool('DATABASE_SSL_SELF', false), // For self-signed certificates
+      },
     },
+    debug: false,
   },
 });
