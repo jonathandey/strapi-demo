e5177e8237f563519efdcd5a12157f44291bfc90
Jonathan Dey <jon@trenttech.co.uk>
2022-08-17 10:48:16 +0100

    Server config update

--- config/server.js
+++ config/server.js
@@ -2,6 +2,6 @@ module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
   app: {
-    keys: env.array('APP_KEYS'),
+    keys: [env('APP_KEY_ONE', ''), env('APP_KEY_TWO', '')],
   },
 });
